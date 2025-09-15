ad1efed73b95fc02089878eabca9477e00fd5577
ManuBruzzone <manubruzzone2@gmail.com>
2025-09-15 11:20:03 -0300

    Arranco ahorcado

--- sala-de-juegos/src/app/pages/ahorcado/ahorcado.ts
+++ sala-de-juegos/src/app/pages/ahorcado/ahorcado.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './ahorcado.css'
 })
 export class Ahorcado {
-
+  palabras: string[] = [
+    "ASADO", "MATE", "EMPANADA", "MILANESA", "CHORIPAN", "FERNET", "ALFAJOR", "TANGO",
+    "FUTBOL", "RIVER", "BOCA", "GIMNASIA", "INDEPENDIENTE", "SANLORENZO", "TUCUMAN",
+    "SALTA", "JUJUY", "CHACO", "FORMOSA", "MENDOZA", "NEUQUEN", "CHUBUT", "SANTAFE",
+    "CORRIENTES", "MISIONES", "CATAMARCA", "SANTIAGO", "SANJUAN", "LARIOJA", "COLECTIVO",
+    "SUBTE", "BOLICHE", "CHE", "PAN", "QUESO", "LECHE", "GALLETAS", "FUTBOL", "AMIGO",
+    "FIESTA", "PLAYA", "MONTAÑA", "AUTO", "CASA", "PERRO", "GATO", "LIBRO", "ESCUELA",
+    "TRABAJO", "CIUDAD"
+  ];
+  palabraElejida: string = '';
+  palabraUsuario: string = '';
 }
